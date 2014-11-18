5a96dbd864cacda4e5bdfbcbcce128fe9c3240b9
Colin Erdman <cerdman@adverseevents.com>
2014-11-18 15:53:17 -0800

    Setup example of restangular with an abstracted service module.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -8,16 +8,21 @@
  * Controller of the angularNewsApp
  */
 angular.module('angularNewsApp')
-  .controller('NewsCtrl', function ($scope, $http) {
+    .controller('NewsCtrl', function ($scope, $http, $log) {
 
-  $http.get('http://api.feedzilla.com/v1/articles.json')
-    .success(function(data){
-    	console.log('data success');
-    	console.log(data); // object seems fine
-    	$scope.articles = data.articles;
-    })
-    .error(function(){
-    	console.log('data error');
+        $http.get('http://api.feedzilla.com/v1/articles.json')
+            .success(function (data) {
+                console.log('data success');
+                console.log(data); // object seems fine
+                $scope.articles = data.articles;
+            })
+            .error(function (err) {
+
+                /***
+                 * use angulars built-in $log facility for this vs. console.log (helps in testing and consistency)
+                 * i.e. $log('data error, got error: '+err)
+                 */
+                $log.debug('Data error, got error: ' + err);
+            });
+
     });
-
-});
